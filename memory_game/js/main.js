fefcc0bf0e0ced6a55d677f59b72a957cb5b8d0a
Tiffany Messer <tiffanymesser15@gmail.com>
2019-07-22 11:23:57 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and Running");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped" + cardFour);
+console.log("User flipped" + cardThree);
