3f63bdfcac12611233bacca03d72ab0f3da06581
Tiffany Messer <tiffanymesser15@gmail.com>
2019-07-23 16:40:12 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,30 @@
 console.log("Up and Running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//console.log("User flipped" + cardFour);
+//console.log("User flipped" + cardThree);
 
-console.log("User flipped" + cardFour);
-console.log("User flipped" + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0]
+cardsInPlay.push('cardOne')
+
+var cardTwo = cards[1]
+cardsInPlay.push('cardTwo')
+
+//console.log(cardsinplay)
+
+console.log("User flipped queen")
+console.log("User flipped king")
+
+if (cardsInPlay[0] === cardsInPlay[1])
+{
+	alert("You found a match!");
+} else {
+	alert("Try again!")
+}
+
+
+console.log('User flipped' + cardOne);
+console.log('User flipped' + cardTwo);
